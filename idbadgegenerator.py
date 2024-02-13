e86ade0fb1154afcc6bfd17d497bf663601ee75e
xtian4zy <xtianidemudia@yahoo.co.uk>
2024-02-13 12:59:01 +0100

    first commit

---
+++ idbadgegenerator.py
@@ -0,0 +1,19 @@
+'''
+Overview
+An ID badge, such as a drivers license or student ID, contains personal details that are formatted in a very specific way.
+
+For this activity you will write a program that will create a properly formatted ID badge.
+'''
+
+print('Please enter the following information: ')
+first_name = input('First Name:')
+last_name = input('Last Name:')
+email = input('Email:')
+phone_num = input('Phone Number:')
+job_title = input('Job Title:')
+id_number = input('ID Number:')
+
+print('The ID Card is: \n-------------------------------')
+
+
+
